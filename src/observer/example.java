10dabf846a03a0b95252c28797785c02161e438c
Salomon Smeke <ssmeke@luc.edu>
2016-04-17 16:10:40 -0500

    started observer

---
+++ src/observer/example.java
@@ -0,0 +1,25 @@
+package observer;
+
+public class example {
+
+	/*
+	 * Use-Case:
+	 * 
+	 * A bar has a list of players who wish to play the piano.
+	 * They dont want to modify their piano, because there are no inherent rules preventing
+	 * anyone from technically playing it. But theyd rather add a set of rules so that
+	 * a drunk cant play it.
+	 * 
+	 * Our Proxy Piano takes in a person with a given BAC. and decides if they can play it or not.
+	 * 
+	 * This additional task, is then not linked to the Piano itself. 
+	 * Which works the same if a drunk plays it or not.
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
