fe5267e477d15e9f7b88caaf44f86ccc4ecb48df
Salomon Smeke <ssmeke@luc.edu>
2016-05-02 17:55:51 -0500

    added line terminator specificity

--- src/builder/CSVConverter.java
+++ src/builder/CSVConverter.java
@@ -2,8 +2,8 @@ package builder;
 
 public class CSVConverter{
 
-	public static String parseInto(String[][] values) {
-		char EOL = '\n';
+	public static String parseInto(String[][] values, String term) {
+		String EOL = term;
 		String out = "";
 		for (int y = 0; y < values.length; y++){
 			for (int x = 0; x < values[y].length; x++){
--- src/builder/Converter.java
+++ src/builder/Converter.java
@@ -1,15 +1,25 @@
 package builder;
 
 public class Converter {
+	public static String lineTerminator = "\n";
 	public static String[] getAvailable(){
 		return new String[]{"CSV","TSV"};
 	}
 	public static String convertInto(String choice, String[][] values){
 		String out = "";
 		if (choice.equals("CSV")){
-			out = CSVConverter.parseInto(values);
+			out = CSVConverter.parseInto(values, lineTerminator);
 		} else {
-			out = TSVConverter.parseInto(values);
+			out = TSVConverter.parseInto(values, lineTerminator);
+		}
+		return out;
+	}
+	public static String convertInto(String choice, String[][] values, String lineTerminator){
+		String out = "";
+		if (choice.equals("CSV")){
+			out = CSVConverter.parseInto(values, lineTerminator);
+		} else {
+			out = TSVConverter.parseInto(values, lineTerminator);
 		}
 		return out;
 	}
